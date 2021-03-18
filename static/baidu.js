e80bdd2389c9a9b38e700f25be260baf5adfa6bc
John-Oula <johnoula@icloud.com>
2021-03-19 00:12:02 +0800

    baidu script

---
+++ static/baidu.js
@@ -0,0 +1,8 @@
+var _hmt = _hmt || [];
+(function () {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?c48c8ab4f09810dc0b9ddec09068a4f0";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
+
