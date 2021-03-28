3e7355a6673d8719ab3132575d28aa0a61d54482
John-Oula <johnoula@icloud.com>
2021-03-29 01:21:31 +0800

    SEO

--- static/javascript.js
+++ static/javascript.js
@@ -187,5 +187,17 @@ if ($('.smart-scroll').length > 0) { // check if element exists
         last_scroll_top = scroll_top;
     });
 }
+
+
+
+ $(window).scroll(function() {
+    $('video').each(function(){
+        if ($(this).is(":in-viewport")) {
+            $(this)[0].play();
+        } else {
+            $(this)[0].pause();
+        }
+    })
+});
 });
 
